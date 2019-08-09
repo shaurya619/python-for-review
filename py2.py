cf409318de42b42af496ead5949af090e6ae1484
Shaurya Bara <38855541+shaurya619@users.noreply.github.com>
2019-08-10 00:26:08 +0530

    Create py2.py

---
+++ py2.py
@@ -0,0 +1,17 @@
+GCI = "GCIisgreat"
+for var in GCI:
+     print(GCI)
+
+
+
+#next program starts below:
+
+a = str(input("ENTER YOUR NAME:"))
+print("Hello!", a, "please to meet you")
+
+newName = a[::-1]
+
+print("Hello!", a, "please to meet you!Did you know that your name backward is", newName)
+
+
+print("Enter To Exit")
